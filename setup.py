aaefe3c789b0cb0f4cafe737e6b25ab35f46b8ec
Chris Barrett <chris.d.barrett@me.com>
2014-04-23 22:38:15 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "dcp",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Chris Barrett",
+    author_email = "chris.d.barrett@me.com",
+    description = "Format a drive for use as a Digital Cinema Package.",
+    license = "BSD3",
+    entry_points = {'console_scripts': ['dcp = dcp.__main__:main',]}
+)
